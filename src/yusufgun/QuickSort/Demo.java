a49b9444f05a45d24867470f535639bef2b34ddf
yusufigday <yusufgun2005@gmail.com>
2026-06-28 18:46:24 +0300

    Add quick sort implementation

---
+++ src/yusufgun/QuickSort/Demo.java
@@ -0,0 +1,42 @@
+package yusufgun.QuickSort;
+
+public class Demo {
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 62, 2, 3, 111, 81, 4, 1};
+
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
